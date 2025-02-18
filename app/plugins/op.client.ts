a19a1b87390bef6523674ce602797f6d1fa294ac
taskylizard <75871323+taskylizard@users.noreply.github.com>
2025-02-18 10:53:08 +0000

    chore: try using official instance

--- app/plugins/op.client.ts
+++ app/plugins/op.client.ts
@@ -7,8 +7,9 @@ export default defineNuxtPlugin(
     enforce: 'pre',
     async setup() {
       const op = new OpenPanel({
-        apiUrl: 'https://op.fmhy.net/api',
-        clientId: '5dec537c-f98f-4465-aef3-6f4a1c583ebc',
+        // apiUrl: 'https://op.fmhy.net/api',
+        // clientId: '5dec537c-f98f-4465-aef3-6f4a1c583ebc',
+        clientId: '3c951636-3f14-4ba6-a449-873fea1d40ec',
         trackScreenViews: true,
         trackOutgoingLinks: true,
         trackAttributes: true
