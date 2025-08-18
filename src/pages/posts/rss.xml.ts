46433e44a3dcbd051b3e6eea270256711e1a2ff5
taskylizard <75871323+taskylizard@users.noreply.github.com>
2025-08-18 22:39:08 +0000

    new website
    WIP

---
+++ src/pages/posts/rss.xml.ts
@@ -0,0 +1,17 @@
+import { getCollection } from 'astro:content'
+import rss from '@astrojs/rss'
+import type { APIRoute } from 'astro'
+import site from '@/site.config'
+
+export const GET: APIRoute = async (context) => {
+	const posts = await getCollection('posts', (post) => post.data.published && !post.data.unlisted)
+	return rss({
+		title: site.title,
+		description: site.description,
+		site: context.site ?? 'http://localhost:4321/',
+		items: posts.map((post) => ({
+			...post.data,
+			link: `/posts/${post.id}/`,
+		})),
+	})
+}
---
+++ src/site.config.ts
@@ -0,0 +1,92 @@
+import type { ImageMetadata } from 'astro'
+
+interface Project {
+	title: string
+	date: Date
+	link: string
+	image?: {
+		light: ImageMetadata
+		dark: ImageMetadata
+	}
+}
+
+interface Link {
+	[key: string]: string
+}
+
+export type SiteConfig = {
+	title: string
+	description: string
+	links: Link
+	projects: Project[]
+}
+
+export default {
+	title: 'tasky',
+	description: 'Shitposter by day, programmer by night.',
+	links: {
+		github: 'taskylizard',
+		bluesky: 'tasky.nuxt.dev',
+	},
+
+	projects: [
+		{
+			title: 'vivivi',
+			link: 'https://trunk.vivivi-647.pages.dev/',
+			date: new Date('2025-06-07'),
+			image: {
+				light: {
+					format: 'webp',
+					height: 1080,
+					width: 1920,
+					src: '/sites/vivivi.webp',
+					orientation: 0,
+				},
+				dark: {
+					format: 'webp',
+					height: 1080,
+					width: 1920,
+					src: '/sites/vivivi.webp',
+					orientation: 0,
+				},
+			},
+		},
+		{
+			title: 'Wotaku',
+			link: 'https://wotaku.wiki/',
+			date: new Date('2023-08-21'),
+			image: {
+				light: {
+					format: 'jpg',
+					height: 1080,
+					width: 1920,
+					src: '/sites/wotaku-light.jpg',
+					orientation: 0,
+				},
+				dark: {
+					format: 'jpg',
+					height: 1080,
+					width: 1920,
+					src: '/sites/wotaku-dark.jpg',
+					orientation: 0,
+				},
+			},
+		},
+		{
+			title: 'freemediaheckyeah',
+			link: 'https://fmhy.net/',
+			date: new Date('2018-04-29'),
+		},
+		{
+			title: 'vyx',
+			link: 'https://github.com/taskylizard/vyx',
+			description: 'Personal discord bot for funsies.',
+			date: new Date('2024-07-13'),
+		},
+		{
+			title: 'privateersclub',
+			link: 'https://megathread.pages.dev',
+			date: new Date('2021-04-14'),
+		},
+	],
+} as SiteConfig
