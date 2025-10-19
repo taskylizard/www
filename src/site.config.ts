9bc222b4fea48574817e52c30f98ff1f0a1991bf
taskylizard <75871323+taskylizard@users.noreply.github.com>
2025-10-19 23:16:44 +0000

    Squash

--- src/site.config.ts
+++ src/site.config.ts
@@ -14,11 +14,18 @@ interface Link {
   [key: string]: string
 }
 
+export interface Friend {
+  name: string
+  link: string
+  image: string
+}
+
 export type SiteConfig = {
   title: string
   description: string
   links: Link
   projects: Project[]
+  friends: Friend[]
 }
 
 export default {
@@ -88,6 +95,68 @@ export default {
       title: 'privateersclub',
       link: 'https://megathread.pages.dev',
       date: new Date('2021-04-14')
+    }
+  ],
+  friends: [
+    {
+      name: 'Alyxia',
+      link: 'https://alyxia.dev',
+      image: 'https://alyxia.dev/static/img/88x31/self.png'
+    },
+    {
+      name: 'Sketchie',
+      link: 'https://sketchni.uk',
+      image: 'https://sketchni.uk/images/sketch.png'
+    },
+    {
+      name: 'Sapphic',
+      link: 'https://sapphic.moe',
+      image: 'https://sapphic.moe/buttons/sapphic.png'
+    },
+    {
+      'name': 'Basil',
+      link: 'https://basil.cafe',
+      image: 'https://sapphic.moe/buttons/friends/basil.gif'
+    },
+    {
+      name: 'Nax',
+      link: 'https://nax.dev',
+      image: 'https://alyxia.dev/static/img/88x31/nax.png'
+    },
+    {
+      name: 'maeve',
+      link: 'https://miaow.ing/',
+      image: 'https://alyxia.dev/static/img/88x31/maeve.png'
+    },
+    {
+      name: 'Ven',
+      link: 'https://vendicated.dev/',
+      image: 'https://sapphic.moe/buttons/friends/ven.png'
+    },
+    {
+      name: 'rushii',
+      link: 'https://rushii.dev',
+      image: 'https://sapphic.moe/buttons/friends/rushii.webp'
+    },
+    {
+      name: 'marsh',
+      link: 'https://marsh.zone/',
+      image: 'https://sapphic.moe/buttons/friends/marsh.png'
+    },
+    {
+      name: 'amy',
+      link: 'https://amy.rip/',
+      image: 'https://amy.rip/88x31.png'
+    },
+    {
+      name: 'paige',
+      link: 'https://codeberg.org/paige',
+      image: 'https://mugman.tech/88x31/paige.gif'
+    },
+    {
+      name: 'mugman',
+      link: 'https://mugman.tech/',
+      image: 'https://mugman.tech/88x31/me.gif'
     }
   ]
 } as SiteConfig
